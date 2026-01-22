91ba0b6de20b8cd1b97cddd89757efc461c350d6
sm <moubaramed@gmail.com>
2026-01-22 22:48:26 +0100

    Fix login loop 500 and mobile header safe area

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,15 +13,15 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         //if ($this->getUser()) {
-           // return $this->redirectToRoute('app_admin');
-         //}
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-   
+
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
 
@@ -31,7 +31,7 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-    
+
     #[Route(path: '/register', name: 'app_register_page')]
     public function register(): Response
     {
